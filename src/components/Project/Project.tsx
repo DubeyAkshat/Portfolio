7ea12f4006a1373dc0fc37c52f912d9b346071c1
Akshat Dubey <iakshatdubey@gmail.com>
2024-03-26 03:18:09 +0530

    Incorrect url in SnapSpeak project

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -73,7 +73,7 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
-                <a href="https://github.com/thisisankit27/SnapSpeak-chat" target="_blank" rel="noreferrer">
+                <a href="https://github.com/thisisankit27/SnapSpeak" target="_blank" rel="noreferrer">
                   GitHub
                 </a>
               </div>
