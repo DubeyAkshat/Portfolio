7c2af9a056e072a07d86f8ec3d6a7a4c7e37d238
Akshat Dubey <iakshatdubey@gmail.com>
2024-03-26 03:13:08 +0530

    Resolve bugs in project section

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -72,16 +72,16 @@ export function Project() {
           <div className="project">
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-              {/* <div className="project-links">
-                <a href="https://github.com/DubeyAkshat/mern-chat" target="_blank" rel="noreferrer">
+              <div className="project-links">
+                <a href="https://github.com/thisisankit27/SnapSpeak-chat" target="_blank" rel="noreferrer">
                   GitHub
                 </a>
-              </div> */}
+              </div>
             </header>
             <div className="body">
               <h3>SnapSpeak</h3>
               <p>
-              AAI powered caption generator & text reader application. Open-sourced the project on GitHub to foster collaboration, knowledge sharing, & community engagement. Individual contribution includes containerization with Docker & Nginx proxy server implemention.
+              An AI powered caption generator & text reader application. Open-sourced the project on GitHub to foster collaboration, knowledge sharing, & community engagement. Individual contribution includes containerization with Docker & Nginx proxy server implemention.
               </p>
             </div>
             <footer>
@@ -105,7 +105,7 @@ export function Project() {
               </div>
             </header>
             <div className="body">
-              <h3>Loan Approval Status Predictor - Machine Learning</h3>
+              <h3>Loan Approval Status Predictor</h3>
               <p>
               Machine learning project focused on predicting loan approval status, leveraging classification algorithms like Decision Trees and Random Forest Trees to provide accurate insights into application outcomes. Also included performance evaluation metrics.
               </p>
